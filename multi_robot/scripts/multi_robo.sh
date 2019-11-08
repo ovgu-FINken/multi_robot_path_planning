d8df1a96e2215266ad65cebba3e7891e7e5dab63
ntraichel <nele.traichel@st.ovgu.de>
2019-11-08 11:33:24 +0100

    Update

--- multi_robot/scripts/multi_robo.sh
+++ multi_robot/scripts/multi_robo.sh
@@ -12,18 +12,28 @@ NUM=0
 # source
 source ~/.bashrc
 source ~/git/DrivingSwarm/devel/setup.bash
-
+source ~/git/DrivingSwarm//setup.bash
 # start tmux
 tmux new-session -s $SESSION_NAME -d
 
 # open world
 tmux rename-window -t $SESSION_NAME "world"
-tmux send-keys -t $SESSION_NAME:$NUM "roslaunch turtlebot3_gazebo turtlebot3_world.launch" C-m
+tmux send-keys -t $SESSION_NAME:$NUM "roslaunch multi_robot simulation.launch" C-m
 
-# random walk
+# amcl
 NUM=$((++NUM))
-tmux new-window -t $SESSION_NAME -n "random"
-tmux send-keys -t $SESSION_NAME:$NUM "roslaunch turtlebot3_gazebo turtlebot3_simulation.launch" C-m
+tmux new-window -t $SESSION_NAME -n "amcl"
+tmux send-keys -t $SESSION_NAME:$NUM "roslaunch multi_robot amcl.launch" C-m
+
+# leader
+NUM=$((++NUM))
+tmux new-window -t $SESSION_NAME -n "leader"
+tmux send-keys -t $SESSION_NAME:$NUM "roslaunch multi_robot leader.launch" C-m
+
+# follower
+NUM=$((++NUM))
+tmux new-window -t $SESSION_NAME -n "follower"
+tmux send-keys -t $SESSION_NAME:$NUM "roslaunch multi_robot follower.launch" C-m
 
 # rviz
 NUM=$((++NUM))
