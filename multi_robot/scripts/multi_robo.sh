cf9328c9bc52c674aa0c49f17405a7e7cf396ceb
johSchm <johSchm@protonmail.com>
2019-11-10 20:39:36 +0100

    Modfied start script

--- multi_robot/scripts/multi_robo.sh
+++ multi_robot/scripts/multi_robo.sh
@@ -7,35 +7,39 @@
 #################################################
 
 SESSION_NAME="multi_robo"
+MAPPING=${MAPPING:=amcl}
 NUM=0
+TERMINAL="gnome-terminal"
 
 # source
 source ~/.bashrc
-source ~/git/DrivingSwarm/devel/setup.bash
-source ~/git/DrivingSwarm//setup.bash
+#source ~/git/DrivingSwarm/devel/local_setup.bash
+
 # start tmux
 tmux new-session -s $SESSION_NAME -d
 
 # open world
 tmux rename-window -t $SESSION_NAME "world"
-tmux send-keys -t $SESSION_NAME:$NUM "roslaunch multi_robot simulation.launch" C-m
+tmux send-keys -t $SESSION_NAME:$NUM "roslaunch multi_robot simulation.launch world:=turtlebot3_world.world" C-m
+read -t 3
 
-# amcl
-NUM=$((++NUM))
-tmux new-window -t $SESSION_NAME -n "amcl"
-tmux send-keys -t $SESSION_NAME:$NUM "roslaunch tracking_uncertainty_grid amcl.launch" C-m
+# navigation
+#NUM=$((++NUM))
+#tmux new-window -t turtlebots -n "navigation"
+#tmux send-keys -t turtlebots:$NUM "roslaunch multi_robot tracking_simulation.launch" C-m
 
 # main
 NUM=$((++NUM))
 tmux new-window -t $SESSION_NAME -n "main"
 tmux send-keys -t $SESSION_NAME:$NUM "roslaunch multi_robot main.launch" C-m
 
+
 # rviz
 NUM=$((++NUM))
 tmux new-window -t $SESSION_NAME -n "rviz"
 tmux send-keys -t $SESSION_NAME:$NUM "roslaunch turtlebot3_gazebo turtlebot3_gazebo_rviz.launch" C-m
 
-# rviz
+# rqt
 NUM=$((++NUM))
 tmux new-window -t $SESSION_NAME -n "rqt"
 tmux send-keys -t $SESSION_NAME:$NUM "rqt" C-m
