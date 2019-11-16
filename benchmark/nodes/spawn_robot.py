c4ffa3b02843e46a1e5df3dcefe94fcb8907c493
johSchm <johSchm@protonmail.com>
2019-11-16 11:26:33 +0100

    Refractored script for ROS 1

--- benchmark/nodes/spawn_robot.py
+++ benchmark/nodes/spawn_robot.py
@@ -7,12 +7,16 @@
 @todo:
 ------------------------------------------------------------- """
 
+import sys
+import roslib
+from gazebo_msgs.srv import SpawnModel, DeleteModel, DeleteModelRequest, SpawnModelRequest
+
+roslib.load_manifest('cob_bringup_sim')
 import rospy
 import os
-import sys
-#import rclpy
-#from ament_index_python.packages import get_package_share_directory
-#from gazebo_msgs.srv import SpawnEntity
+from geometry_msgs.msg import *
+import tf.transformations as tft
+from std_msgs.msg import Empty as EmptyMsg
 
 
 NODE_NAME = "spawn_robot"
@@ -23,78 +27,135 @@ DEFAULT_NAME = "0"
 DEFAULT_NAMESPACE = "tb3_"
 
 
-def node_logger(node, text):
-    """ Logs a text for a node.
-    :param node:
-    :param text:
+def get_obj_pose(position, orientation):
+    """ Returns the pose of the object.
+    :param position:
+    :param orientation:
+    :return: pose
     """
-    if node is not None:
-        node.get_logger().info(text)
+    quaternion = tft.quaternion_from_euler(orientation[0], orientation[1], orientation[2])
+    object_pose = Pose()
+    object_pose.position.x = float(position[0])
+    object_pose.position.y = float(position[1])
+    object_pose.position.z = float(position[2])
+    object_pose.orientation.x = quaternion[0]
+    object_pose.orientation.y = quaternion[1]
+    object_pose.orientation.z = quaternion[2]
+    object_pose.orientation.w = quaternion[3]
+    return object_pose
 
 
-def create_node(name):
-    """ Creates a node.
-    :param name
-    :return node
+def get_file_location(model, model_type):
+    """ Returns the file location path.
+    :param model:
+    :param model_type:
+    :return: path
     """
-    rclpy.init()
-    node = rclpy.create_node(name)
-    node_logger(node, 'Creating Service client to connect to `/' + name + 'spawn_entity`')
-    return node
+    try:
+        file_location = roslib.packages.get_pkg_dir(
+            'cob_gazebo_objects') + '/objects/' + model + '.' + model_type
+    except:
+        raise FileNotFoundError("File not found: cob_gazebo_objects"
+                                + "/objects/" + model + "." + model_type)
+    return file_location
 
 
-def create_client(node, name):
-    """ Creates a client.
-    :param node:
-    :param name:
-    :return: client
+def get_model(file_path):
+    """ Call gazebo service to spawn model (see http://ros.org/wiki/gazebo).
+    :param file_path:
+    :return: srv_model, xml_string
     """
-    client = node.create_client(SpawnEntity, "/" + name)
-    node_logger(node, "Connecting to `/" + name + "` service...")
-    if not client.service_is_ready():
-        client.wait_for_service()
-        node_logger(node, "...connected!")
-    return client
-
-
-def spawn_robot(_model_type, _name_space, _name, _position):
-    """ Spawns a turtlebot.
-    :param _model_type:
-    :param _name_space:
-    :param _name:
-    :param _position:
-    """
-    node = create_node(NODE_NAME)
-    client = create_client(node, NODE_NAME)
-
-    sdf_file_path = os.path.join(
-        get_package_share_directory("turtlebot3_gazebo"), "models",
-        "turtlebot3_", model_type, "model.sdf")
-
-    request = SpawnEntity.Request()
-    request.name = _name
-    request.xml = open(sdf_file_path, 'r').read()
-    request.robot_namespace = _name_space
-    request.initial_pose.position.x = float(_position[0])
-    request.initial_pose.position.y = float(_position[1])
-    request.initial_pose.position.z = float(_position[2])
-
-    node.get_logger().info("Sending service request to `/spawn_entity`")
-    future = client.call_async(request)
-    rclpy.spin_until_future_complete(node, future)
-    if future.result() is not None:
-        print('response: %r' % future.result())
+    if model_type == "urdf":
+        srv_spawn_model = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
+        file_xml = open(file_path)
+        xml_string = file_xml.read()
+    elif model_type == "urdf.xacro":
+        p = os.popen("rosrun xacro xacro.py " + file_path)
+        xml_string = p.read()
+        p.close()
+        srv_spawn_model = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
+    elif model_type == "model":
+        srv_spawn_model = rospy.ServiceProxy('/gazebo/spawn_gazebo_model', SpawnModel)
+        file_xml = open(file_path)
+        xml_string = file_xml.read()
     else:
-        raise RuntimeError(
-            'exception while calling service: %r' % future.exception())
+        raise ValueError('Model type not know. model_type = ' + model_type)
+    return srv_spawn_model, xml_string
 
-    node.get_logger().info("Done! Shutting down node.")
-    node.destroy_node()
-    rclpy.shutdown()
+
+def delete_robot(name):
+    """ Deletes a robot.
+    :param name:
+    :return: request
+    """
+    srv_delete_model = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+    req = DeleteModelRequest()
+    req.model_name = name
+    exists = True
+    try:
+        res = srv_delete_model(name)
+    except rospy.ServiceException as e:
+        exists = False
+        rospy.logdebug("Model %s does not exist in gazebo.", name)
+    if exists:
+        rospy.loginfo("Model %s already exists in gazebo. Model will be updated.", name)
+    return req
+
+
+def spawn_model(name, namespace, model, pose, world):
+    """ Spawns a model in a simulation world.
+    :param name:
+    :param namespace:
+    :param model:
+    :param pose:
+    :param world:
+    """
+    rospy.wait_for_service('gazebo/spawn_sdf_model')
+    spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
+    spawn_model_prox(name, model, namespace, pose, world)
+
+
+def spawn_robot(name, namespace, model_type,
+                position, orientation, update_if_exist=False):
+    """ Spawns a robot.
+    :param name:
+    :param namespace:
+    :param model:
+    :param model_type:
+    :param position:
+    :param orientation:
+    :param update_if_exist:
+    """
+    rospy.init_node(NODE_NAME)
+    rospy.sleep(5)
+    rospy.wait_for_service("/gazebo/spawn_urdf_model")
+
+    pose = get_obj_pose(position, orientation)
+    file_path = get_file_location(name, model_type)
+    srv_spawn_model, xml_string = get_model(file_path)
+
+    if update_if_exist:
+        delete_robot(name)
+
+    spawn_model(name, namespace, srv_spawn_model, pose, world="world")
+
+
+def loop():
+    """ Loop until shutdown.
+    """
+    sim = rospy.get_param('/use_sim_time')
+    if sim is True:
+        rospy.loginfo('Running in simulation, publishing to /sim_spawned topic')
+        pub = rospy.Publisher('/sim_spawned', EmptyMsg, latch=True)
+        pub.publish(EmptyMsg())
+        pub.publish(EmptyMsg())
+        pub.publish(EmptyMsg())
+        rospy.spin()
 
 
 if __name__ == "__main__":
-    """ Main method. """
+    """ Main method.
+    """
     model_type = rospy.get_param('~model_type', DEFAULT_MODEL_TYPE)
     number_of_robots = rospy.get_param('~number_of_robots', DEFAULT_NUMBER_OF_ROBOTS)
     namespace = rospy.get_param('~namespace', DEFAULT_NAMESPACE)
@@ -102,6 +163,8 @@ if __name__ == "__main__":
 
     for i in range(number_of_robots):
         spawn_robot(
-            _model_type=model_type, _name_space=namespace,
-            _position=position, _name=str(i))
+            model_type=model_type, namespace=namespace,
+            position=position, orientation=[0, 0, 0],
+            name=str(i), update_if_exist=False)
+    loop()
 
