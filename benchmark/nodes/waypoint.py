20dd5240af7b08c7dbec2d77e7ff6a7b6d953996
johSchm <johSchm@protonmail.com>
2019-11-17 10:47:23 +0100

    Updated waypoint manager.

--- benchmark/nodes/waypoint.py
+++ benchmark/nodes/waypoint.py
@@ -8,7 +8,19 @@
 ------------------------------------------------------------- """
 
 
+import actionlib
 from enum import Enum
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import tf.transformations as tft
+import random
+import math
+from std_msgs.msg import Float32MultiArray
+
+
+TOPIC_NAME = "waypoint"
+NODE_NAME = "waypoint_publisher"
+DEFAULT_NODE_UPDATE_FREQUENCY = 3
 
 
 class WayPointMap(Enum):
@@ -27,30 +39,86 @@ class WayPointMap(Enum):
 
 class WayPointManager:
     """ Way point manager.
+
+    This class is responsible for publishing
+    the current target waypoint to the waypoint node.
+    Therefore, each robot has its very own waypoint node.
     """
 
-    def __init__(self, way_points=WayPointMap.EMPTY_WORLD):
+    def __init__(self, namespace, robot_names,
+                 waypoints=WayPointMap.EMPTY_WORLD,
+                 node_update_frequency=DEFAULT_NODE_UPDATE_FREQUENCY):
         """ Init. method.
-        :param way_points
+        :param namespace
+        :param robot_names
+        :param waypoints
+        :param node_update_frequency
         """
-        self._way_points = way_points
+        if type(waypoints) is WayPointMap:
+            self._waypoint_map = waypoints.value
+        else:
+            self._waypoint_map = waypoints
+
+        self._namespace = namespace
+        self._robot_names = robot_names
+        self._target_point = {}
+        self._node_update_frequency = node_update_frequency
+        self._publisher = {}
+        self._setup_node()
+        self._setup_publisher()
 
     def run(self):
         """ Starts the way point generating process.
         """
-        if self._way_points == WayPointMap.EMPTY_WORLD \
-                or self._way_points == WayPointMap.EMPTY_WORLD.value:
-            self._set_way_points_empty_world()
-        elif self._way_points == WayPointMap.EDGE_TB3_WORLD \
-                or self._way_points == WayPointMap.EDGE_TB3_WORLD.value:
-            self._set_way_points_tb3_world()
+        self._loop()
 
-    def _set_way_points_empty_world(self):
-        """ Sets the way points for the empty world.
+    @staticmethod
+    def _setup_node():
+        """ Setup method for the way point node.
+        Here the current way point is published.
         """
+        rospy.init_node(NODE_NAME, anonymous=True)
 
-    def _set_way_points_tb3_world(self):
-        """ Sets the way points for the tb3 world.
+    def _loop(self):
+        """ Updates the node with the given frequency.
         """
+        rate = rospy.Rate(self._node_update_frequency)
+        while not rospy.is_shutdown():
+            self._update_target_points()
+            self._publish_target_points()
+            rate.sleep()
 
+    def _setup_publisher(self):
+        """ Setup for the waypoint topics.
+        """
+        for robot_name in self._robot_names:
+            topic_name = self._namespace + '_' + robot_name + '/' + TOPIC_NAME
+            pub = rospy.Publisher(topic_name, Float32MultiArray, queue_size=10)
+            self._publisher[robot_name] = pub
 
+    def _publish_target_points(self):
+        """ Updates the target point for the robot
+        by publishing the new waypoints to the topics.
+        """
+        for robot_name in self._robot_names:
+            target_point = self._target_point[robot_name]
+            self._publisher[robot_name].publish(target_point)
+
+    def _update_target_points(self):
+        """ Updates the target points for the robots.
+        """
+        for robot_name in self._robot_names:
+
+            # initial target point
+            if robot_name not in self._target_point:
+                self._target_point[robot_name] = self._waypoint_map[0]
+
+            # restart round
+            elif self._target_point[robot_name] == self._waypoint_map[len(self._waypoint_map)]:
+                self._target_point[robot_name] = self._waypoint_map[0]
+
+            # set next in round
+            else:
+                current_target_idx = self._waypoint_map.index(self._target_point[robot_name])
+                target_point = self._waypoint_map[current_target_idx + 1]
+                self._target_point[robot_name] = target_point
