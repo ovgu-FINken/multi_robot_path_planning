6b16accc7887722ea025447049d16a74a864b8bd
johSchm <johSchm@protonmail.com>
2019-11-18 11:28:25 +0100

    Added robot names param pass to waypoint manager

--- benchmark/nodes/waypoints_controller.py
+++ benchmark/nodes/waypoints_controller.py
@@ -10,21 +10,25 @@
 
 import rospy
 import waypoint as wp
-from std_msgs.msg import Int16MultiArray, Int16
+from std_msgs.msg import Int16MultiArray
 import topic_handler
 
 
 DEFAULT_NAMESPACE = "tb3_"
 
 
+robot_names = []
+
+
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    global robot_names
+    robot_names = [str(name) for name in data.data]
 
 
 wp.setup_node()
 topic_handler.SubscribingHandler("robot_names", Int16MultiArray, callback)
-rospy.spin()
 
 namespace = rospy.get_param('~namespace', DEFAULT_NAMESPACE)
-wp_manager = wp.WayPointManager(namespace=namespace, robot_names=["0", "1", "2", "3"])  # @HACK
+wp_manager = wp.WayPointManager(namespace=namespace, robot_names=robot_names)
 wp_manager.run()
