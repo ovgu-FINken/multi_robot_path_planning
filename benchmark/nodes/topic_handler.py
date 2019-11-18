d53a75278f929ebee4cbd95bfa5355c2d7b32760
johSchm <johSchm@protonmail.com>
2019-11-18 10:47:33 +0100

    Extended topic handler to also support subscribing

--- benchmark/nodes/topic_handler.py
+++ benchmark/nodes/topic_handler.py
@@ -12,8 +12,43 @@ from std_msgs.msg import Int16MultiArray, Int16
 import rospy
 
 
+def subscriber_callback(data):
+    """ Callback method for subscriber.
+    :param data:
+    :return: data
+    """
+    return data.data
+
+
 class TopicHandler:
     """ ROS Topic Handler.
+    """
+
+    def __init__(self, name, data_type):
+        """ Init. method
+        :param name: name of the topic
+        :param data_type (eg: Int16MultiArray)
+        """
+        self._name = name
+        self._data_type = data_type
+
+
+class SubscribingHandler(TopicHandler):
+    """ Subscribing Handler.
+    """
+
+    def __init__(self, name, data_type, callback=subscriber_callback):
+        """ Init. method
+        :param name: name of the topic
+        :param data_type: (eg: Int16MultiArray)
+        :param callback: for data
+        """
+        super().__init__(name, data_type)
+        self._subscriber = rospy.Subscriber(self._name, self._data_type, callback)
+
+
+class PublishingHandler(TopicHandler):
+    """ Publishing Handler.
     """
 
     def __init__(self, name, data_type, queue_size=10):
@@ -22,9 +57,8 @@ class TopicHandler:
         :param data_type (eg: Int16MultiArray)
         :param queue_size
         """
-        self._name = name
-        self._data_type = data_type
-        self._publisher = rospy.Publisher(self._name, data_type, queue_size=queue_size)
+        super().__init__(name, data_type)
+        self._publisher = rospy.Publisher(self._name, self._data_type, queue_size=queue_size)
 
     def publish(self, data, frequency=0, quiet=True):
         """ Publishes data to the topic
