3cfdfd731d695105cc7a45017482a69c3b4b736a
johSchm <johSchm@protonmail.com>
2019-12-13 11:32:39 +0100

    Pretty print + added setup infos

--- benchmark/nodes/src/utils/log.py
+++ benchmark/nodes/src/utils/log.py
@@ -11,6 +11,8 @@
 import rospkg
 import datetime
 import src.settings as settings
+import calendar
+import time
 
 
 DEFAULT_LOG_DIRECTORY = rospkg.RosPack().get_path('benchmark') + "/log/eval_log.txt"
@@ -81,7 +83,9 @@ class Logger:
         all necessary information into the log file.
         """
         sm = settings.SettingsManager()
-        text = "\n---##--- BENCHMARK ---##---\n" \
+        text = "\n\n---------------------- BENCHMARK ----------------------\n" \
+               + "==> datetime: " + str(datetime.datetime.now()) + "\n" \
+               + "==> Benchmark ID: " + str(calendar.timegm(time.gmtime())) + "\n" \
                + "==> model_name: " + str(sm.read("model_name")) + "\n" \
                + "==> model_type: " + str(sm.read("model_type")) + "\n" \
                + "==> namespace: " + str(sm.read("namespace")) + "\n" \
@@ -91,5 +95,6 @@ class Logger:
                + "==> orientation: " + str(sm.read("orientation")) + "\n" \
                + "==> wp_map: " + str(sm.read("wp_map")) + "\n" \
                + "==> wp_threshold: " + str(sm.read("wp_threshold")) + "\n" \
-               + "==> world: " + str(sm.read("world"))
+               + "==> world: " + str(sm.read("world")) + "\n" \
+               + "--------------------------------------------------------\n"
         self._write(text, timestamp=False)
