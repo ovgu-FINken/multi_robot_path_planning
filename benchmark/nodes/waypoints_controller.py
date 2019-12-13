034b1a95f477c7e8f7b9b616873f97bd7e7df179
johSchm <johSchm@protonmail.com>
2019-12-13 22:58:59 +0100

    fixed wp param pass

--- benchmark/nodes/waypoints_controller.py
+++ benchmark/nodes/waypoints_controller.py
@@ -78,19 +78,20 @@ def setup_odometry_subscriber(_number_of_robots, _namespace):
         topic_handler.SubscribingHandler(odom_name, Odometry, callback_odometry, robot_id)
 
 
-def update_wps(_number_of_robots, _namespace,
+def update_wps(_number_of_robots, _namespace, _rounds,
                _wp_map, _wp_threshold, frequency=0.5):
     """ Updates the waypoints for the robots.
     :param _number_of_robots:
     :param _wp_map:
+    :param _rounds:
     :param _namespace:
     :param frequency:
     :param _wp_threshold:
     """
     wp_manager = wp.WayPointManager(
         namespace=_namespace, number_of_robots=_number_of_robots,
-        callback=callback_target, waypoints=_wp_map,
-        threshold=wp_threshold)
+        wp_callback=callback_target, round_callback=callback_rounds,
+        waypoints=_wp_map, threshold=wp_threshold, rounds=_rounds)
     while not rospy.is_shutdown():
         wp_manager.update(robot_current_positions)
         rospy.Rate(frequency).sleep()
@@ -104,7 +105,8 @@ namespace = rospy.get_param('namespace')
 wp_map = rospy.get_param('wp_map')
 wp_threshold = rospy.get_param('wp_threshold')
 number_of_robots = rospy.get_param('number_of_robots')
+rounds = rospy.get_param('rounds')
 setup_waypoint_publisher(target_publisher, number_of_robots, namespace)
 setup_rounds_publisher(rounds_publisher, number_of_robots, namespace)
 setup_odometry_subscriber(number_of_robots, namespace)
-update_wps(number_of_robots, namespace, wp_map, wp_threshold)
+update_wps(number_of_robots, namespace, rounds, wp_map, wp_threshold)
