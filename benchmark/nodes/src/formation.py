19270bfd5a29607a8c83b79533210ba563ced2d8
johSchm <johSchm@protonmail.com>
2019-12-31 11:47:39 +0100

    Fixed dense block formation bug for 1 or 2 robots

--- benchmark/nodes/src/formation.py
+++ benchmark/nodes/src/formation.py
@@ -62,18 +62,42 @@ class FormationHandler:
         """ Runs the computation process for a dense block formation
         and returns the positions and orientations.
         """
-        block_dimension = int(math.sqrt(self._number_of_robots))
-        block_rest = self._number_of_robots - 2 * block_dimension
-        num_rows = block_dimension + (0 if block_rest == 0 else 1)
-        for row in range(num_rows):
-            row_pos = row * self._distance
-            num_col = block_dimension if row < block_dimension else block_rest
-            for col in range(num_col):
-                col_pos = col * self._distance
-                self._position.append([
-                    row_pos + self._center_point[0],
-                    col_pos + self._center_point[1], 0.0])
-                self._orientation.append([0.0, 0.0, 0.0])
+        if self._number_of_robots == 1 or self._number_of_robots == 2:
+            self._handle_trivial_dense_block()
+        else:
+            block_dimension = int(math.sqrt(self._number_of_robots))
+            block_rest = self._number_of_robots - 2 * block_dimension
+            num_rows = block_dimension + (0 if block_rest == 0 else 1)
+            for row in range(num_rows):
+                row_pos = row * self._distance
+                num_col = block_dimension if row < block_dimension else block_rest
+                for col in range(num_col):
+                    col_pos = col * self._distance
+                    self._position.append([
+                        row_pos + self._center_point[0],
+                        col_pos + self._center_point[1], 0.0])
+                    self._orientation.append([0.0, 0.0, 0.0])
+
+    def _handle_trivial_dense_block(self):
+        """ Sets the postion and the orientation for trivial dense block
+        formations, like for one or two robots.
+        """
+        if self._number_of_robots == 1:
+            self._position.append(self._center_point)
+            self._orientation.append([0.0, 0.0, 0.0])
+        elif self._number_of_robots == 2:
+            self._position.append([
+                self._center_point[0],
+                self._center_point[1],
+                0.0
+            ])
+            self._orientation.append([0.0, 0.0, 0.0])
+            self._position.append([
+                self._center_point[0] + self._distance,
+                self._center_point[1],
+                0.0
+            ])
+            self._orientation.append([0.0, 0.0, 0.0])
 
     def _estimate_at_way_points(self):
         """ Runs the computation process for a at way points formation
