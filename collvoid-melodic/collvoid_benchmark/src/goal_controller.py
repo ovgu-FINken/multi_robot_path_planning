a73cbb627bf38f9629bbf36dd598112a7426bb65
ntraichel <nele.traichel@st.ovgu.de>
2020-04-01 23:08:17 +0200

    added loggings for debugging and info; changed goal transfer to communication via topic ..simple/goal due to non-working service version (see issues)

--- collvoid-melodic/collvoid_benchmark/src/goal_controller.py
+++ collvoid-melodic/collvoid_benchmark/src/goal_controller.py
@@ -9,9 +9,9 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseActionGoal, MoveBaseActio
 
 class GoalController:
     def __init__(self):
-        # self.pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=0)
+        self.pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=0)
         ns = rospy.get_namespace()
-        topic_name = ns + "/benchmark/waypoint"
+        topic_name = ns + "benchmark/waypoint"
         self.sub = rospy.Subscriber(topic_name, Point, self.catch_waypoint_cb)
 
         self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
@@ -23,6 +23,7 @@ class GoalController:
         '''
         @brief: enhance waypoint with orientation, frame_id and timestamp
         '''
+        rospy.loginfo("Received goal: %f, %f, %f", data.x, data.y, data.z)
         self.cntr = 0
         waypoint = data
         nextGoal = PoseStamped()
@@ -34,19 +35,31 @@ class GoalController:
         nextGoal.pose.orientation.z = 0
         nextGoal.pose.orientation.w = 1
 
-        self.forward_goal(nextGoal)
+        # self.forward_goal_as_service(nextGoal)
+        self.forward_goal_as_message(nextGoal)
 
-    def forward_goal(self, nextGoal):
+    def forward_goal_as_message(self, nextGoal):
+        goal = PoseStamped()
+        goal = nextGoal
+        self.pub.publish(goal)
+        rospy.loginfo("Forwarded goal: %f, %f, %f", goal.pose.position.x, goal.pose.position.y, goal.pose.position.z)
+
+    def forward_goal_as_service(self, nextGoal):
         '''
         @brief: send goal using service API of move_base
         '''
+
         goal = MoveBaseActionGoal()
         goal = nextGoal
-        self.client.send_goal()
+        self.client.send_goal(goal)
+        rospy.loginfo("Forwarded goal: %f, %f, %f", goal.pose.position.x, goal.pose.position.y, goal.pose.position.z)
+
         self.client.wait_for_result()
-        # result = self.client.get_result()
+        result = self.client.get_result()
+        rospy.loginfo("Result: %s", result)
 
         if self.cntr < 3 and self.client.get_state() == actionlib.GoalStatus.ABORTED:
+            rospy.logerr("Goal could not be reached, try again...")
             self.cntr += 1
             self.forward_goal(goal)
         elif self.client.get_state() == actionlib.GoalStatus.SUCCEEDED:
@@ -57,6 +70,7 @@ class GoalController:
 
 if __name__ == '__main__':
     rospy.init_node('goal_controller', anonymous=True)
+    rospy.loginfo("Goal controller initiated.")
 
     try:
         GoalController()
