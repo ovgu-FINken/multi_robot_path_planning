1c1720bc301f04cea1bf2c66af42f5f8b92e584f
Kilian <kilian7195@web.de>
2020-04-07 19:33:37 +0200

    fixes

--- RobotPathCostmap/RobotPathCostmap/src/navigation_paths.cpp
+++ RobotPathCostmap/RobotPathCostmap/src/navigation_paths.cpp
@@ -1,8 +1,11 @@
 #include <string>
 #include <nav_msgs>
 #include <list>
+#include <vector>
 #include <cmath>
 #include "./../include/RobotPathCostmap/navigation_paths.h">
+
+using namespace std;
 
 // ToDo: Libs einbinden
 // ToDo: launch-file
@@ -14,17 +17,16 @@
 namespace navigation_path_layers 
 {
 
-    static double* gauss_a = 1;
     static const double gauss_sigma = 1.0;
 	static const double  gauss_r, gauss_s = 2.0 * gauss_sigma * gauss_sigma;
 
 void NavigationPathLayer::onInitialize()
 {
 
-    bool* side_inflation = false;
-	int* filter_strength = 1; 
-    int* inflation_size = 1;
-    int* filter_size = 9; // resolution: 0.050000 meters / pixel ; 4 pixel needed for robot itself
+    bool side_inflation = false;
+	int filter_strength = 1; 
+    int inflation_size = 1;
+    int filter_size = 9; // resolution: 0.050000 meters / pixel ; 4 pixel needed for robot itself
     first_time_ = true;
     ros::NodeHandle nh("~/" + name_), g_nh; // ToDo: check cooperation with other Multi-robot-path-planning-files
     paths_sub_ = nh.subscribe("/paths", 1, &NavigationPathLayer::pathCallback, this);
@@ -62,8 +64,8 @@ void NavigationPathLayer::pathCallback(const nav_msgs::Path& path)
         paths_list_.push(path);
     } else
     {
-        std::string oldPath = paths_list_[index_].poses.toString();
-        std::string newPath = path.poses.toString();
+        string oldPath = paths_list_[index_].poses.toString();
+        string newPath = path.poses.toString();
         if (oldpath.compare(newpath) != 0)
         {
             changed = true;
@@ -99,10 +101,10 @@ void NavigationPathLayer::updateBounds( double* min_x, double* min_y, double* ma
   else
   {
     double a = *min_x, b = *min_y, c = *max_x, d = *max_y;
-    *min_x = std::min(last_min_x_, *min_x);
-    *min_y = std::min(last_min_y_, *min_y);
-    *max_x = std::max(last_max_x_, *max_x);
-    *max_y = std::max(last_max_y_, *max_y);
+    *min_x = min(last_min_x_, *min_x);
+    *min_y = min(last_min_y_, *min_y);
+    *max_x = max(last_max_x_, *max_x);
+    *max_y = max(last_max_y_, *max_y);
     last_min_x_ = a;
     last_min_y_ = b;
     last_max_x_ = c;
@@ -119,10 +121,10 @@ void NavigationPathLayer::updateCosts()
 
     for(nav_msgs::Path& path: navigation_path_layers::paths_list_)
     {
-        std::vector positions<std::vector<int>> positions;
+        list <vector<int>> positions;
         for(geometry_msgs::PoseStamped pose_ : path.poses)
         {
-            std::vector<int> position = {pose_.pose.position.x, pose_.pose.position.y};
+            vector<int> position = {pose_.pose.position.x, pose_.pose.position.y};
             positions.push(position)
         }
         // add cost hills per path
@@ -130,36 +132,36 @@ void NavigationPathLayer::updateCosts()
     }
 }
 
-void NavigationPathLayer::setSideInflation(bool* inflate)
+void NavigationPathLayer::setSideInflation(bool inflate)
 {
-    navigation_path_layers::side_inflation = *inflate;
+    navigation_path_layers::side_inflation = inflate;
     navigation_path_layers::createFilter();
 }
 
-void NavigationPathLayer::scaleSideInflation(double* inflation_scale)
+void NavigationPathLayer::scaleSideInflation(double inflation_scale)
 {
     // maximal factor 1 of the costs of the normal path
-    navigation_path_layers::inflation_size = std::min(*inflation_scale, 1);
+    navigation_path_layers::inflation_size = min(inflation_scale, 1);
     navigation_path_layers::createFilter();
 }
 
-void NavigationPathLayer::setFilterSize(int* size)
+void NavigationPathLayer::setFilterSize(int size)
 {
     if (size%2 == 1) 
     {
-    navigation_path_layers::filter_size = *size;
+    navigation_path_layers::filter_size = size;
     } else
     {
         // minimum size required
-        navigation_path_layers::filter_size = std::max(*size-1, 9); // ~ 12.5 cm tolerance with degrading costs
+        navigation_path_layers::filter_size = max(size-1, 9); // ~ 12.5 cm tolerance with degrading costs
     }
     
     // navigation_path_layers::createFilter();
 }
 
-void NavigationPathLayer::setFilterStrength(int* s)
+void NavigationPathLayer::setFilterStrength(int s)
 {
-    navigation_path_layers::filter_strength = *s;
+    navigation_path_layers::filter_strength = s;
     // navigation_path_layers::createFilter();
 }
 
@@ -174,14 +176,14 @@ void NavigationPathLayer::resetCosts()
     // gesamte Layer auf 0 zurücksetzen
 }
 
-costmap_2d::Costmap2D* NavigationPathLayer::createCostHillChain(std::vector<std::vector<int>> positions, costmap_2d::Costmap2D* costmap) // Pfad übergeben
+costmap_2d::Costmap2D* NavigationPathLayer::createCostHillChain(list<vector<int>> positions, costmap_2d::Costmap2D costmap) // Pfad übergeben
 {
-	costmap_2d::Costmap2D* costmap_ = costmap;
+	costmap_2d::Costmap2D costmap_ = costmap;
     // increase costs along the path
 	for (unsigned int pos = 0; pos < positions.size(); i++)
 	{
-		std::vector<int> position = positions[pos];
-		costmap_ = useFilter(position*, costmap_);
+		vector<int> position = positions[pos];
+		costmap_ = useFilter(position, costmap_);
 	}
  
 	// add later
@@ -199,8 +201,8 @@ void NavigationPathLayer::createFilter() // Größe und side_inflation nutzen
 {
 	// sum is for normalization 
 	double sum = 0.0;
-	int bound = int((*filter_size - 1) / 2);
-	double[][] * kernel = (double*)malloc(*filter_size * sizeof(double));
+	int bound = int((filter_size - 1) / 2);
+	double[][] kernel = double[filter_size][filter_size];
 
 	for (unsigned int i = -bound; i <= bound; i++)
 	{
@@ -212,17 +214,17 @@ void NavigationPathLayer::createFilter() // Größe und side_inflation nutzen
 		}
 	}
 
-	if (*side_inflation)
+	/*if (side_inflation)
 	{
         // ToDo: add later
 		// add values to "right" side of robot
 		// sum += these values
-	}
+	}*/
 
 	// normalising the Kernel 
-	for (int i = 0; i < *filter_size; ++i)
+	for (int i = 0; i < filter_size; ++i)
 	{
-		for (int j = 0; j < *filter_size; ++j)
+		for (int j = 0; j < filter_size; ++j)
 		{
 			kernel[i][j] /= sum;
 		}
@@ -231,22 +233,22 @@ void NavigationPathLayer::createFilter() // Größe und side_inflation nutzen
 	return kernel;
 }
 
-costmap_2d::Costmap2D* useFilter(std::vector<int>* position, costmap_2d::Costmap2D* costmap)
+costmap_2d::Costmap2D useFilter(vector<int> position, costmap_2d::Costmap2D costmap)
 {
 	int bound = int((filter_size - 1) / 2);
-	costmap_2d::Costmap2D _map = *costmap; // ToDo prove
+	costmap_2d::Costmap2D _map = costmap; // ToDo prove
 
 	// for each pixel in the convolution take maximum value of current and calculated value of convolution at this pixel
 	for (unsigned int i = -bound; i <= bound; i++)
 	{
 		for (unsigned int j = -bound; j <= bound; j++)
 		{
-			double current = costmap[*position[0] + i][*position[1] + j];
-			_map[*position[0] + i][*position[1] + j] = std::max(current, kernel[i][j] * filter_strength);
+			double current = costmap[position[0] + i][position[1] + j];
+			_map[position[0] + i][position[1] + j] = max(current, kernel[i][j] * filter_strength);
 		}
 	}
 
-    return _map*;
+    return _map;
 }
 
 }
